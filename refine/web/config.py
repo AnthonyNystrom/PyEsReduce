bd3a27809f6d4833cc10ae6c8bfdc55c121207be
Anthony Nyström <nystrom.anthony@gmail.com>
2014-07-04 15:55:29 -0500

    Initial Commit

---
+++ refine/web/config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+DEBUG=True
+SECRET_KEY='development key'
+
+WEB_HOST='0.0.0.0'
+WEB_PORT=8888
+UPLOAD_FOLDER='/root/refine/jobs/refine/'
+
+REDIS_HOST='0.0.0.0'
+REDIS_PORT=6379
+REDIS_PASS='surfikiMR'
