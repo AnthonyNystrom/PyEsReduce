89be602e0a584151daf7cef4b538f12c75b31ad0
Curt <zhongting.hu@gmail.com>
2014-07-05 18:44:50 -0700

    change the config add the gitignore

--- refine/web/config.py
+++ refine/web/config.py
@@ -5,8 +5,8 @@
 DEBUG=True
 SECRET_KEY='development key'
 
-WEB_HOST='0.0.0.0'
-WEB_PORT=8888
+WEB_HOST='ec2-54-88-9-158.compute-1.amazonaws.com'
+WEB_PORT=80
 UPLOAD_FOLDER='/root/refine/jobs/refine/'
 
 REDIS_HOST='0.0.0.0'
--- server_startup.sh
+++ server_startup.sh
@@ -3,5 +3,5 @@ PYTHONPATH=$PYTHONPATH:/root
 PYTHONPATH=$PYTHONPATH:/root/refine
 PYTHONPATH=$PYTHONPATH:/root/refine/jobs/refine
 export PYTHONPATH
-python /root/refine/startup.py
-python /root/refine/tools/mapper_watcher.py &
+python ./startup.py
+python ./tools/mapper_watcher.py &
